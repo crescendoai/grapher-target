f16f5c041bbf0b2100baa0bed1a967e7d5c89620
bobpappas <bob.pappas@ayasdi.com>
2018-05-16 16:02:47 -0700

    Adding more code to see if PhantomJS breaks in Travis.

--- spec/targetSpec.js
+++ spec/targetSpec.js
@@ -1,6 +1,38 @@
 describe('a grapher instance with target', function () {
- 
-  it('trying to find why jasmine breaks', function() {
+  var network, grapher;
+  var index = 1;
+  const MAX_PERFORMANCE_NODES = 10000;
+  const MAX_PERFORMANCE_LINKS = 10000;
+  const MAX_X = 2000;
+  const MAX_Y = 2000;
+  const MAX_R = 50;
+  
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  function generateNodes(max) {
+    let nodes = [max];
+    for (let i = 0; i < max; i++) {
+      nodes[i] = { x: getRandomInt(MAX_X),
+                   y: getRandomInt(MAX_Y),
+                   r: getRandomInt(MAX_R)
+                 }
+    }
+    return nodes;
+  }
+
+  function generateLinks(max) {
+    let links = [max];
+    for (let i = 0; i < max; i++) {
+      links[i] = { from: getRandomInt(max),
+                    to: getRandomInt(max)
+                 }
+    }
+    return links;
+  }
+   
+  it('trying to find why PhantomJS breaks in Travis', function() {
     expect(0).toBe(0);
   })
 
