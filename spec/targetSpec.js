d0be2190038ff2b3d8cb384606e30d4702f2f47e
bobpappas <bob.pappas@ayasdi.com>
2018-05-16 16:06:49 -0700

    Ok, that last set of code broke, trimming it down! Could it be const?

--- spec/targetSpec.js
+++ spec/targetSpec.js
@@ -6,32 +6,7 @@ describe('a grapher instance with target', function () {
   const MAX_X = 2000;
   const MAX_Y = 2000;
   const MAX_R = 50;
-  
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
 
-  function generateNodes(max) {
-    let nodes = [max];
-    for (let i = 0; i < max; i++) {
-      nodes[i] = { x: getRandomInt(MAX_X),
-                   y: getRandomInt(MAX_Y),
-                   r: getRandomInt(MAX_R)
-                 }
-    }
-    return nodes;
-  }
-
-  function generateLinks(max) {
-    let links = [max];
-    for (let i = 0; i < max; i++) {
-      links[i] = { from: getRandomInt(max),
-                    to: getRandomInt(max)
-                 }
-    }
-    return links;
-  }
-   
   it('trying to find why PhantomJS breaks in Travis', function() {
     expect(0).toBe(0);
   })
